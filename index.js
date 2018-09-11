db5d5e0d915d9a2f822b9c7e04146fe6d38eb4d4
acdcoll <gomezrosmel@gmail.com>
2018-09-10 23:51:38 -0400

    Prueba de Test-INtegration Rosmel Gomez

--- index.js
+++ index.js
@@ -1,39 +1,95 @@
-var express = require('express'),
- 	bodyParser = require('body-parser'),
- 	http = require('http'),
- 	app,
- 	router,
- 	server;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var request = require('request');
+var sha1 = require('sha1');
+var propertiesreader = require('properties-reader')
+var properties = propertiesreader('database.properties')
+var mysql = require('mysql')
+var bodyParser = require('body-parser');
 
-var incryp = require('node-sha1'); // falta por la incriptacion title
+var Host = properties.get('Host')
+var Port = properties.get('Port')
+var Username = properties.get('Username')
+var Password = properties.get('Password')
+var Database = properties.get('Database')
 
- app = express();
- app.use(bodyParser.json());
+var connection = mysql.createConnection({
+    host: Host,
+    user: Username,
+    password: Password,
+    database: Database,
+    port: Port
+})
 
- router = express.Router();
- router.get('/', function (req, res){
- 	res.json([
 
- 				{
-					"userId": 1,
-					"id": 1,
-					"title": "quidem molestiae enim",
-					"hash": "ba232411feb13a64634e52d1e39183c9185af197"
-				},
 
-				{
-					"userId": 1,
-				    "id": 2,
-				    "title": "sunt qui excepturi placeat culpa",
-					"hash": "d2a09f04fc7557af48b5cab5b930592dd1e9c06c"
-				}
+app.use(bodyParser.json())
 
- 			]);
-	});
+app.get('/', function (req, res) {
 
-app.use('/admision', router);
+    url = 'http://jsonplaceholder.typicode.com/albums';
 
-server = http.createServer(app);
-server.listen(3000, function() {
-    console.log('localhost:3000/admision');
+    request(url, function(error, response, json){
+
+	var j = JSON.parse(json)
+
+	for(var object in j) {
+		j[object].hash=sha1(j[object].title)
+	}
+
+	res.send(j)
+
+    })
+
+
 });
+
+app.get('/post', function(req, res) {
+    var requestData = {
+        "nombre": "Rosmel",
+	"apellido": "Gomez",
+	"correo": "rosmelgabrielgomez@gmail.com"
+    }
+
+    request({
+        url: 'http://localhost:3000/post',
+        method: "POST",
+    	json: requestData,
+        multipart: {
+            chunked: false,
+            data: [
+                {
+                    'content-type': 'application/json',
+                    body: requestData
+                }
+            ]
+        }
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log('request ok')
+        }
+        else {
+            console.log("error: " + error)
+
+        }
+    })
+
+    res.send('success inserting persona')
+
+
+});
+
+app.post('/post', function (req, res) {
+
+
+  	var query = "INSERT INTO persona (nombre, apellido, correo) VALUES (req.body.nombre, req.body.apellido, req.body.correo)";
+	connection.query(query, function (err, result) {
+    		console.log("data inserted into persona");
+  	});
+
+   	res.send(req.body);
+});
+
+app.listen(port);
+console.log('Server started! At http://localhost:3000')
