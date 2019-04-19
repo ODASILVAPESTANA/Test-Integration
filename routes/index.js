b07679edcc467083bac846bbe42f3f14520a47f2
Alexis Canchica <alenux1@gmail.com>
2019-04-18 21:28:36 -0400

    add news files evidencia and prueba_admision, the last file is get data personal and database name

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 const request = require("request");
 var crypto = require('crypto');
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+      host     : 'db4free.net',
+      user     : 'dbuser',
+      password : 'sql9144354',
+      database : 'prueba_admision'
+
+});
 
 let objJson;
 
@@ -23,7 +31,6 @@ router.get('/admision', function(req, res, next) {
               res.status(200).send( JSON.stringify(data, null, 4));
     });
    }catch (error){
-      //res.status(304).send({error : error.toString() });
       console.log(error);
    }
 });
@@ -36,7 +43,7 @@ var  getJsonUrl = function  (callback ){
     }else {
       objJson = JSON.parse(JSON.stringify(datos));
       for (var i=0;i<objJson.length;i++){
-         objJson[i].hash= crypto.createHash("sha1").update(JSON.stringify(objJson[i])).digest("hex");
+         objJson[i].hash= crypto.createHash("sha1").update(objJson[i].title).digest("hex");
       }
       return callback(null,objJson);
     }
