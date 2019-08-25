653ae9292833341fc70a8766e2984bfbb2b16513
Alexander Ramirez <aramirez@Alexanders-MacBook-Air.local>
2019-08-25 01:01:45 -0400

    HTTP GET admision done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const sha1 = require('sha1');
 const app = express();
 
 app.listen(3000, () => {
@@ -7,5 +8,18 @@ app.listen(3000, () => {
 });
 
 app.get('/admision', (req, res) => {
-	request('http://jsonplaceholder.typicode.com/albums', (error, response, body) => res.send(body));
+	request('http://jsonplaceholder.typicode.com/albums', (error, response, body) => {
+		if (response.statusCode === 200) {
+			body = JSON.parse(body);
+			body.forEach((album) => {
+				album.hash = sha1(album.title);
+			});
+			res.send(body);
+		} else {
+			res.status(404);
+			res.send({
+				msg : 'Ha ocurrido un error en el servidor',
+			});
+		}
+	});
 });
